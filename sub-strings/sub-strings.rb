5c9774668b4f70f26e2b95caa9635973a1f608d5
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-01-26 13:32:13 -0500

    Add spacing

--- sub-strings/sub-strings.rb
+++ sub-strings/sub-strings.rb
@@ -4,14 +4,18 @@ def substrings(text, dictionary)
 
   dictionary.each do |dictionary_word|
     dictionary_word.downcase!
+
     if text.include? dictionary_word
       count = 0
+
       text.split.each do |word_to_check|
         count += 1 if word_to_check.include? dictionary_word
       end
+
       if count > 0
           matches[dictionary_word] = count
       end
+
     end
   end
   matches
