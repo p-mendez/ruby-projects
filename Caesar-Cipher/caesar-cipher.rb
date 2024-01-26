fc22b0814c145f6a2479632d3b19e54025b24261
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-01-25 23:35:36 -0500

    Complete get_shifted_alphabet

--- Caesar-Cipher/caesar-cipher.rb
+++ Caesar-Cipher/caesar-cipher.rb
@@ -1,15 +1,28 @@
-def caesar_cipher(string, shift)
+# Returns a encrypted caesar cipher of message with applied shift
+# Params:
+# - message: string
+# - shift: positive integer
+def caesar_cipher(message, shift)
   alphabet = [*'a'..'z']
   shifted_alphabet = get_shifted_alphabet(shift)
-  p alphabet
-  p shifted_alphabet
+  shifted_string = message
+
+  message.gsub(/\w/, shifted_alphabet)
 end
 
+# Returns hash of characters, regular_alphabet => shifted_alphabet
+# Params:
+# - shift: integer
 def get_shifted_alphabet(shift)
   alphabet = [*'a'..'z']
-  alphabet[shift..-1] << alphabet[0..shift -1]
+  shifted_values = alphabet[shift..-1] + alphabet[0..shift -1]
+  shifted_alphabet = {}
+
+  alphabet.each_with_index do |letter, i|
+    shifted_alphabet[letter] = shifted_values[i]
+    shifted_alphabet[letter.upcase] = shifted_values[i].upcase
+  end
+  shifted_alphabet
 end
 
-caesar_cipher("What a string!", 5) #=> "Bmfy f xywnsl!"
-
-caesar_cipher("z", 1)
+# p caesar_cipher("What a string!", 5) #=> "Bmfy f xywnsl!"
