77503319434c107c695bcb5d5de24ef2fc36a8ef
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-01-25 22:48:26 -0500

    Refactor get shifted alphabet

--- Caesar-Cipher/caesar-cipher.rb
+++ Caesar-Cipher/caesar-cipher.rb
@@ -1,10 +1,13 @@
 def caesar_cipher(string, shift)
   alphabet = [*'a'..'z']
-  shifted_alphabet = alphabet[shift..-1] << alphabet[0..shift -1]
+  shifted_alphabet = get_shifter_alphabet(alphabet)
   p alphabet
   p shifted_alphabet
 end
 
+def get_shifter_alphabet(alphabet) do
+  alphabet[shift..-1] << alphabet[0..shift -1]
+end
 
 caesar_cipher("What a string!", 5) #=> "Bmfy f xywnsl!"
 
