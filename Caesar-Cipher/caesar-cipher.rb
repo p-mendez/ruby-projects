d1702ed4ff219bce4656f89ce95080a05cfa0158
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-01-25 22:51:10 -0500

    Fix get_shifted_alphabet fn

--- Caesar-Cipher/caesar-cipher.rb
+++ Caesar-Cipher/caesar-cipher.rb
@@ -1,11 +1,12 @@
 def caesar_cipher(string, shift)
   alphabet = [*'a'..'z']
-  shifted_alphabet = get_shifter_alphabet(alphabet)
+  shifted_alphabet = get_shifted_alphabet(shift)
   p alphabet
   p shifted_alphabet
 end
 
-def get_shifter_alphabet(alphabet) do
+def get_shifted_alphabet(shift)
+  alphabet = [*'a'..'z']
   alphabet[shift..-1] << alphabet[0..shift -1]
 end
 
