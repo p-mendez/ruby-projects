f404b49f18716437de00b039e60b2155dc6d5258
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-04-08 21:31:37 -0400

    Plan out steps for caesar cipher

---
+++ caesar-cipher/caesar-cipher.rb
@@ -0,0 +1,11 @@
+
+
+
+def caesar_cipher(string, shift)
+  # create new hash: shifted alphabet
+  # initialize blank new_string
+  # loop through new_string and translate to shifted_alphabet
+end
+
+puts caesar_cipher("What a string!", 5)
+# "Bmfy f xywnsl!"
