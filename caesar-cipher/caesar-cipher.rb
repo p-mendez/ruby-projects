b9d7ac2c33a3bb065a9e19ec06b37b24cb95188a
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-04-08 23:01:10 -0400

    Complete assignment using hashes

--- caesar-cipher/caesar-cipher.rb
+++ caesar-cipher/caesar-cipher.rb
@@ -1,19 +1,15 @@
 def caesar_cipher(string, shift)
-  # create new hash: shifted alphabet
   dict = shift_alphabet(shift)
-  # loop through new_string and translate to dict
   cipher(string, dict)
 end
 
 def shift_alphabet(shift)
   dict = {}
 
-  # values __ - z
   97.upto(122 - shift) do |num|
     dict[num.chr] = (num + shift).chr
   end
 
-  # values a - ___
   shift.times do |num|
     dict[(123 - shift + num).chr] = (97 + num).chr
   end
