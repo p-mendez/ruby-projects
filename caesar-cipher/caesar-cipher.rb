aea6aed2c80549c6373102f98b4a87cb255c4f2c
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-04-08 23:12:04 -0400

    Change logic using String#tr

--- caesar-cipher/caesar-cipher.rb
+++ caesar-cipher/caesar-cipher.rb
@@ -1,43 +1,14 @@
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.freeze
+
 def caesar_cipher(string, shift)
-  dict = shift_alphabet(shift)
-  cipher(string, dict)
+  shifted = shift_alphabet(shift)
+  string.tr(ALPHABET, shifted).tr(ALPHABET.upcase, shifted.upcase)
 end
 
 def shift_alphabet(shift)
-  dict = {}
-
-  97.upto(122 - shift) do |num|
-    dict[num.chr] = (num + shift).chr
-  end
-
-  shift.times do |num|
-    dict[(123 - shift + num).chr] = (97 + num).chr
-  end
-
-  dict
+  ALPHABET.chars.rotate(shift).join
 end
 
-def cipher(str, dict)
-  ciphered_string = String.new
-  str.each_char do |char|
-    ciphered_string +=
-      if dict.include? char.downcase
-        shift_letter_using(dict, char)
-      else
-        char
-      end
-  end
-
-  ciphered_string
-end
-
-def shift_letter_using(dict, char)
-  if char == char.upcase
-    dict[char.downcase].upcase
-  else
-    dict[char]
-  end
-end
 
 puts caesar_cipher("What a string!", 5)
 # "Bmfy f xywnsl!"
