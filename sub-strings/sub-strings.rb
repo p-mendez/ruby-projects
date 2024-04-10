629235f1a92ef0bdc1009a11eb4f54b8293a1e60
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-04-09 22:27:58 -0400

    Complete second try

--- sub-strings/sub-strings.rb
+++ sub-strings/sub-strings.rb
@@ -16,3 +16,19 @@ end
 
 # p substrings("below", dictionary)
 # p substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+# 2nd try:
+
+# def substrings(string, dict)
+#   # convert string to array
+#   str_arr = string.split
+#   matches = Hash.new
+
+#   # loop through each dictionary word and count
+#   dict.each do |dict_word|
+#     count = str_arr.count { |str_arr_element| str_arr_element.downcase.include? dict_word }
+#     matches[dict_word] = count if count.positive?
+#   end
+
+#   matches
+# end
